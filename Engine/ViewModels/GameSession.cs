9e6d7d57510d62234c8ef8bba6c0281e2fb5f00d
Victor Ajani <ajavi1@qc.cgi.ca>
2019-04-05 14:07:54 -0400

    bind game obects to View

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -4,17 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace Engine.ViewModels
 {
     public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
+            // All these objects will bind to the view
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Victor";
+            CurrentPlayer.CharacterClass = "NinjaDude";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
             CurrentPlayer.Gold = 1000000;
         }
     }
--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Engine.ViewModels;
 
 namespace WPFUI
 {
@@ -20,9 +22,15 @@ namespace WPFUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GameSession _gameSession; // private var
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _gameSession = new GameSession(); // instantiate new game session
+
+            DataContext = _gameSession;
         }
     }
 }

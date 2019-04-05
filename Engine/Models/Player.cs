71fada043bccc102608b74bb284939f47e45c7fb
Victor Ajani <ajavi1@qc.cgi.ca>
2019-04-05 11:16:10 -0400

    Player class

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    class Player
+    public class Player
     {
+        string Name { get; set; } // You can "set" a value to Player.Name. You can also "get" the value of Player.Name from somewhere else.
+        string CharacterClass { get; set; }
+        int HitPoints { get; set; }
+        int ExperiencePoints { get; set; }
+        int Level { get; set; }
+        int Gold { get; set; }
     }
 }

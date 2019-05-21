560dad980901d4c9f39b15d20d5b37391cbddbfa
Victor Ajani <portneufcoder@gmail.com>
2019-05-20 20:55:13 -0400

    Event args

---
+++ Engine/EventArgs/GameMessageEventArgs.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Engine.EventArgs
+{
+    public class GameMessageEventArgs : System.EventArgs
+    {
+        public string Message { get; private set; }
+
+        public GameMessageEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+}
